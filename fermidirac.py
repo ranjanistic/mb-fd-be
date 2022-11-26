81903cb8efe0712e09d1d35fcff99283de35f02a
ranjanistic <priyanshuranjan88@gmail.com>
2022-11-26 18:11:10 +0530

    update fd

--- fermidirac.py
+++ fermidirac.py
@@ -1,5 +1,10 @@
 def distribute(e, n):
-  return e+n
+  dist = {};
+  for i in range(0,e+1):
+    dist[f"L{i}"] = []
+    for j in range(1,n+1):
+      dist[f"L{i}"].append(j)
+  return dist
 
 e = int(input("Energy levels:"))
 n = int(input("Particles:"))
